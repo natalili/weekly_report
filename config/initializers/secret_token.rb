c669e1ad965b7d7e213b43949ba6a09d48473639
natalili <natali.li@list.ru>
2012-04-24 15:43:01 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeeklyReport::Application.config.secret_token = 'd263e569ee3aa8918c76cca4f8f603055d538a8aad884d99c630d6e3c46d03423b5ee86b7c80b1541d8038b015f4a70d68eda8567178126f3d6e194390b4a0b4'
