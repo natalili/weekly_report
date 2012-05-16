86b8558c6c810735b9a6ed45eef5e389f9543afa
natalili <natali.li@list.ru>
2012-05-16 12:57:57 +0300

    next

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+# -*- encoding : utf-8 -*-
 module ApplicationHelper
+  
+  # return a title on a per-page basis.
+  def title
+    base_title = "Информация регистратора"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
