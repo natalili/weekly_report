607d1d0ae349b4b85559c381e033adacdfa2d062
natalili <natali.li@list.ru>
2012-05-18 18:16:07 +0300

    routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 WeeklyReport::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
+  match '/contact', :to => 'pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
